f931e061df5a7d633045a75f3b65cd21ea9f7744
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-25 20:02:17 +0200

    Refactor pokemon cards and style it

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,31 +10,29 @@ const Home = () => {
     getPokemons().then((response) => setPokemons(response));
   }, []);
 
-  const renderList = (pokemons) => {
-    return pokemons.map((pokemon) => {
-      return (
-        <Link to={`/pokemon/${pokemon.id}`} key={pokemon.id}>
-          <li className="card">
-            <img src={pokemon.image} alt={pokemon.name} />
-            <h2>{pokemon.name}</h2>
-            <ul>
-              {pokemon.types.map((type, index) => {
-                return (
-                  <li key={index}>
-                    <p>{type}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </li>
-        </Link>
-      );
-    });
+  const renderCard = (pokemon) => {
+    return (
+      <div className="card">
+        <div className="card__image">
+          <img src={pokemon.image} alt={pokemon.name} />
+        </div>
+        <div className="card__footer">
+          <h2 className="card__title">{pokemon.name}</h2>
+          <span className="card__types">{pokemon.types.join(', ')}</span>
+        </div>
+      </div>
+    );
   };
 
   return (
     <>
-      <ul className="list">{renderList(pokemons)}</ul>
+      <ul className="list">
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <Link to={`/pokemon/${pokemon.id}`}>{renderCard(pokemon)}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
