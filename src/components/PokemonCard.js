cfc4ea6cb0d77a17357d12855fa480ab8b420bae
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-29 22:36:31 +0200

    Create pokemon card component

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,19 @@
+import './PokemonCard.scss';
+
+function PokemonCard(props) {
+  const { pokemon } = props;
+
+  return (
+    <div className="card">
+      <div className="card__image">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <div className="card__footer">
+        <h2 className="card__title">{pokemon.name}</h2>
+        <span className="card__types">{pokemon.types.join(', ')}</span>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonCard;
