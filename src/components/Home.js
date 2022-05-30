c6064a0a02dfb4306f0aca8062a0911b7cf96df5
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-30 21:59:31 +0200

    Add loading and create render content

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,22 +21,23 @@ const Home = () => {
   const [pokemons, setPokemons] = useState([]);
   const [filterName, setFilterName] = useState('');
   const [pokemonsCount, setPokemonsCount] = useState();
+  const [loading, setLoading] = useState(false);
   const [offset, setOffset] = useState(0);
   const limit = 20;
 
   useEffect(() => {
+    setLoading(true);
     getPokemons({
       limit: String(limit),
       offset: String(offset),
       search: filterName.toLowerCase(),
     }).then((response) => {
       setPokemonsCount(response.count);
-
       const newPokemons = filterName
         ? response.items
         : sort(duplicates([...pokemons, ...response.items]), 'id');
-
       setPokemons(newPokemons);
+      setLoading(false);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [offset, filterName]);
@@ -45,19 +46,20 @@ const Home = () => {
     setOffset(offset + limit);
   };
 
-  if (!pokemons.length) {
-    return (
-      <div className="home-container">
-        <div className="loading">
-          <Loading />
+  const renderContent = () => {
+    if (loading && !pokemons.length) {
+      return (
+        <div className="home-container">
+          <div className="loading">
+            <Loading />
+          </div>
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="home-container">
-      <Filters setFilterName={setFilterName} filterName={filterName} />
+      );
+    }
+    if (!loading && !pokemons.length) {
+      return <h1>There are no pokemons!</h1>;
+    }
+    return (
       <div role="list">
         <InfiniteScroll
           className="list"
@@ -79,6 +81,12 @@ const Home = () => {
           ))}
         </InfiniteScroll>
       </div>
+    );
+  };
+  return (
+    <div className="home-container">
+      <Filters setFilterName={setFilterName} filterName={filterName} />
+      {renderContent()}
     </div>
   );
 };
